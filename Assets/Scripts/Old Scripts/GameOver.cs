088289fb9c5d879f5def073034698f33835669c3
Lumina <tsunayoshihibari@gmail.com>
2023-04-27 11:03:11 +0800

    更新：坦克勋章ui(未完成)，调整关卡结束ui(未完成)

--- Assets/Scripts/Old Scripts/GameOver.cs
+++ Assets/Scripts/Old Scripts/GameOver.cs
@@ -1,15 +1,23 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
-    public Button reSetGame;
+    [SerializeField] private Button _nextStage;
+    [SerializeField] private Button _home;
     // Start is called before the first frame update
     void Start()
     {
-        reSetGame = GetComponentInChildren<Button>();
-        reSetGame.onClick.AddListener(ResetScene);
+        _nextStage = GameObject.Find("btn_retry").GetComponent<Button>();
+        _nextStage.onClick.AddListener(ResetScene);
+        _home = GameObject.Find("btn_ads").GetComponent<Button>();
+        _home.onClick.AddListener(BackToHome);
+    }
+
+    private void BackToHome()
+    {
     }
 
     private void ResetScene()
